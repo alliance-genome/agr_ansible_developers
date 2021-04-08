0ebca960810d4a86fbb6d27cbec8855b04c1c6ed
Manuel Luypaert <luypaert@ebi.ac.uk>
2021-04-08 18:19:47 +0100

    Added support for AWS_PROFILE env variable to password-client.sh script (to support use of named AWS profiles)

--- password-client.sh
+++ password-client.sh
@@ -1,2 +1,12 @@
-secret1=$(docker run --rm -it -v ~/.aws:/root/.aws amazon/aws-cli secretsmanager get-secret-value --region us-east-2 --secret-id AnsibleDevelopers --version-stage AWSCURRENT --query SecretString --output text | grep -o '"[^"]\+"' | sed 's/"//g')
+GET_SECRET_CMD="docker run --rm -it -v ${HOME}/.aws:/root/.aws amazon/aws-cli"
+
+#Pass on AWS profile env variable
+if [ ! -z ${AWS_PROFILE} ]; then
+	GET_SECRET_CMD="${GET_SECRET_CMD} --profile ${AWS_PROFILE}"
+fi
+
+GET_SECRET_CMD="${GET_SECRET_CMD} secretsmanager get-secret-value --region us-east-2 --secret-id AnsibleDevelopers --version-stage AWSCURRENT --query SecretString --output text"
+
+secret1=$(${GET_SECRET_CMD} | grep -o '"[^"]\+"' | sed 's/"//g')
+
 echo ${secret1}
