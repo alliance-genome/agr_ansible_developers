af8c468d927f03fde1eb2fcee6f636fc5d710a10
Christopher Tabone <christabone@gmail.com>
2021-04-08 13:46:06 -0300

    Removed unnecessary ENV stuff.

--- password-client.sh
+++ password-client.sh
@@ -1,3 +1,2 @@
 secret1=$(docker run --rm -it -v ~/.aws:/root/.aws amazon/aws-cli secretsmanager get-secret-value --region us-east-2 --secret-id AnsibleDevelopers --version-stage AWSCURRENT --query SecretString --output text | grep -o '"[^"]\+"' | sed 's/"//g')
-export ENV1=${secret1}
-echo ${ENV1}
+echo ${secret1}
