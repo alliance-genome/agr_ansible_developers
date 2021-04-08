28aa8f2b2a251cd6b5f49e83da26723c00e29096
Manuel Luypaert <luypaert@ebi.ac.uk>
2021-04-08 18:29:03 +0100

    Removed additional secret output parsing (value now stored in clean format in AWS)

--- password-client.sh
+++ password-client.sh
@@ -5,8 +5,4 @@ if [ ! -z ${AWS_PROFILE} ]; then
 	GET_SECRET_CMD="${GET_SECRET_CMD} --profile ${AWS_PROFILE}"
 fi
 
-GET_SECRET_CMD="${GET_SECRET_CMD} secretsmanager get-secret-value --region us-east-2 --secret-id AnsibleDevelopers --version-stage AWSCURRENT --query SecretString --output text"
-
-secret1=$(${GET_SECRET_CMD} | grep -o '"[^"]\+"' | sed 's/"//g')
-
-echo ${secret1}
+${GET_SECRET_CMD} secretsmanager get-secret-value --region us-east-2 --secret-id AnsibleDevelopers --query SecretString --output text
