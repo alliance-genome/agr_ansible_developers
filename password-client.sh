b1993d09295cd0d1f5b2597ed404c98057fa4e8d
Manuel Luypaert <luypaert@ebi.ac.uk>
2021-04-09 11:58:55 +0100

    Reverted to json-like secret format (hidden characters added by AWS otherwise) + added retrieval failure check to prevent silent failures

--- password-client.sh
+++ password-client.sh
@@ -5,4 +5,10 @@ if [ ! -z ${AWS_PROFILE} ]; then
 	GET_SECRET_CMD="${GET_SECRET_CMD} --profile ${AWS_PROFILE}"
 fi
 
-${GET_SECRET_CMD} secretsmanager get-secret-value --region us-east-2 --secret-id AnsibleDevelopers --query SecretString --output text
+${GET_SECRET_CMD} secretsmanager get-secret-value --region us-east-2 --secret-id AnsibleDevelopers --query SecretString --output text \
+  | grep -o '"[^"]\+"' | sed 's/"//g'
+
+if [ $? -ne 0 ]; then
+	>&2 echo -e "Error while retrieving secret: ${secret1}\n"
+	exit 1
+fi
